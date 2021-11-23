f1d10abd8edf42621d04fe0fc08a8ba9c4e50deb
dhinesh <dhinesh5124@gmail.com>
2021-11-23 12:09:29 +0530

    bug fixes

--- src/authentication/getUserId.js
+++ src/authentication/getUserId.js
@@ -2,8 +2,10 @@ import Cookies from "js-cookie"
 import jwt_decode from "jwt-decode";
 
 const token = Cookies.get('token')
-const data = jwt_decode(token)
 
 export function getUserId() {
-    return data.jti
+    if(token) {
+        const data = jwt_decode(token)
+        return data.jti
+    }
 }
